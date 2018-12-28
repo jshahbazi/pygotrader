5d8ec0adfabae2aa6daf90d1ed722d0642fdbf16
John Shahbazian <jshahbazi@hotmail.com>
2018-12-28 20:35:59 +0000

    Added product argument for coin choice

--- src/pygotrader/cli.py
+++ src/pygotrader/cli.py
@@ -5,12 +5,13 @@ import os
 from pygotrader import tui
 
 
-exchange_choices = ['coinbase']
+exchanges = ['coinbase']
+products = ['BTC-USD','ETH-USD','LTC-USD']
 
 class ExchangeArgumentAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         exchange = values
-        if exchange.lower() not in exchange_choices:
+        if exchange.lower() not in exchanges:
             parser.error("Unknown exchange")
         namespace.exchange = exchange.lower()
 
@@ -21,12 +22,18 @@ class SecretsArgumentAction(Action):
             pass
         else:
             parser.error("Secrets file does not exist")
+            
+class ProductArgumentAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        product = values
+        if product.upper() not in products:
+            parser.error("Unknown product")
+        namespace.product = product.upper()            
 
 def create_parser():
     parser = ArgumentParser(description="")
-    exchange_arg_help = f"Exchange to connect to.  Currently supported exchanges: {exchange_choices}"
     parser.add_argument("--exchange", 
-        help=exchange_arg_help,
+        help=f"Exchange to connect to.  Currently supported exchanges: {exchanges}",
         metavar=("EXCHANGE"),
         action=ExchangeArgumentAction,
         required=True)
@@ -34,7 +41,12 @@ def create_parser():
         help="File that contains API secrets required to connect to your exchange",
         metavar=("SECRETS_FILE"),
         action=SecretsArgumentAction,
-        required=True)        
+        required=True)  
+    parser.add_argument("--product", 
+        help=f"Coin pairing to trade with.  Currently supported products: {products}",
+        metavar=("PRODUCT"),
+        action=ProductArgumentAction,
+        required=True) 
     return parser
     
 class CustomExit(Exception):
@@ -45,8 +57,16 @@ def signal_handler(signum, frame):
     print('Caught signal %d' % signum)
     raise CustomExit
 
+
+
+class MyConfig(object):
+    def __init__(self,exchange='coinbase',product='BTC-USD'):
+        self.exchange = exchange
+        self.product = product
+
+
 def main():
-    
+
     signal.signal(signal.SIGTERM, signal_handler)
     signal.signal(signal.SIGINT, signal_handler)
     
@@ -54,14 +74,10 @@ def main():
     
         args = create_parser().parse_args()
         
-        if args.exchange == 'coinbase':
-            mytui = tui.TerminalDisplay()
-            curses.wrapper(mytui.display_loop)
-        else:
-            pass
+        my_config = MyConfig(exchange=args.exchange,product=args.product)
+
+        mytui = tui.TerminalDisplay()
+        curses.wrapper(mytui.display_loop)
         
     except CustomExit:
-        #TODO handle threads exiting here
         pass
-    
-    print(f"Exiting...")
