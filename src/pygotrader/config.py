1e6f94eb8424c52829ef3ce7662df1d235335189
John Shahbazian <jshahbazi@hotmail.com>
2019-01-01 22:13:42 +0000

    Added loading of secrets file.  Added method to load authenticated coinbase client

--- src/pygotrader/config.py
+++ src/pygotrader/config.py
@@ -1,13 +1,48 @@
 """
 Class for holding all of the current configuration information
-So far:
+ and loading secrets for connections to exchanges.  Also creates
+ authenticated clients.
+ 
+Secrets file format:
 
-exchange
-product
+{"key":"12345",
+ "b64secret":"1l2o3o4n5g",
+ "passphrase":"pass123",
+ "api_url":"https://api.pro.coinbase.com"
+}
+
 """
 
+import json,os
+from cbpro import AuthenticatedClient
 
 class MyConfig(object):
     def __init__(self,exchange='coinbase',product='BTC-USD'):
-        self.exchange = exchange
-        self.product = product
+        self._exchange = exchange
+        self._product = product
+        self._key = ''
+        self._b64secret = ''
+        self._passphrase = ''
+        self._api_url = ''
+
+    @property
+    def exchange(self):
+        return self._exchange
+        
+    @property
+    def product(self):
+        return self._product
+        
+    def load_secrets(self,path_to_secrets_file):
+        secrets_file = open(path_to_secrets_file)
+        secrets = json.load(secrets_file)
+        self._key = secrets['key']
+        self._b64secret = secrets['b64secret']
+        self._passphrase = secrets['passphrase']
+        self._api_url = secrets['api_url']
+        
+    def get_coinbase_authenticated_client(self):
+        return AuthenticatedClient(key=self._key, 
+                                b64secret=self._b64secret,
+                                passphrase=self._passphrase,
+                                api_url=self._api_url)
